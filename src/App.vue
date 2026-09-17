962ed2e4f81e95449fb8d11a726095698ef6b97a
Josecarlos Lusbel <jlusbel@gmail.com>
2026-09-16 20:47:25 -0400

    Added LoginView.vue component along with fontawesome dependency for icons.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import WorkoutView from './components/WorkoutView.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        WholeFit
+      </a>
+    </div>
+  </nav>
+  <div class="columns">
+    <div class="column is-2">
+      <aside class="menu">
+        <p class="menu-label">General</p>
+        <ul class="menu-list">
+          <li><a>Calendar</a></li>
+          <li><a>Progress</a></li>
+        </ul>
+      </aside>
+    </div>
+  </div>
 </template>
---
+++ src/components/WorkoutView.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+
+const exerciseOptions = ref([
+    {value: "benchPress", label: "Bench Press", category: "strength"},
+    {value: "shoulderPress", label: "Shoulder Press", category: "strength"},
+    {value: "bicepCurls", label: "Bicep Curls", category: "strength"},
+    {value: "treadmill", label: "Treadmill", category: "cardio"}
+])
+
+const exercises = ref([{exerciseName: "", sets: 0, reps: 0, duration: ""}])
+</script>
+
+<template>
+    <div class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <div class="modal-card-head">
+                <p class="modal-card-title">Workout Details</p>
+                <button class="delete"></button>
+            </div>
+            <div class="modal-body">
+                <p>Enter your exercises below.</p>
+                <table class="table is-bordered is-striped">
+                    <thead>
+                        <tr>
+                            <th>Exercise</th>
+                            <th>Sets</th>
+                            <th>Reps</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(exercise, index) in exercises" :key="index">
+                            <td>
+                                <div class="select is-info">
+                                    <select v-model="exercise.exerciseName">
+                                        <option 
+                                            v-for="option in exerciseOptions" 
+                                            :value="option.value"
+                                        >
+                                        {{ option.label }}
+                                        </option>
+                                    </select>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
